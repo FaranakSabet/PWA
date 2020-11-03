0be70dc48528f9d257f1ebe99797d257627541c9
faranak Sabet <faranaksabet@yahoo.com>
2020-11-02 21:38:57 -0800

    update sw.js

--- public/sw.js
+++ public/sw.js
@@ -10,3 +10,23 @@ const assets = [
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ]
+// install event
+self.addEventListener("install", (evt) => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log("caching shell assets")
+        cache.addAll(assets)
+      })
+    )
+  })
+  // activate event
+self.addEventListener("activate", (evt) => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+      caches.keys().then((keys) => {
+        //console.log(keys);
+        return Promise.all(keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key)))
+      })
+    )
+  })
